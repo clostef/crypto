7eb18d80b2dd333f49eb03eda6b5703f7214ee03
clovis stefanutti <c.stefanutti@outlook.fr>
2025-05-28 18:55:21 +0200

    feat/2-login-user-code

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
-function HomePage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white text-3xl">
-      Bienvenue sur la page d'accueil 🏠
-    </div>
-  );
-}
-
 function App() {
   return (
     <Routes>
