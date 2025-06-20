85749c2dc7124f518a7bacbdccd7ccab2ce8dd67
clovis stefanutti <c.stefanutti@outlook.fr>
2025-06-20 14:19:09 +0200

    feat/11-display-menu-items

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../features/user/userSlice";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { LogOut } from "lucide-react";
 import CryptoCard from "../components/CryptoCard";
 import WalletSummary from "../components/WalletSummary";
 import CryptoChartCard from "../components/CryptoChartCard";
 import CryptoLogo from "../assets/crypto/crypto-logo.png";
 import ProfileLogo from "../assets/crypto/profile-logo.png";
+import LayoutIcon from "../assets/icons/layout-board.svg";
+import MarketIcon from "../assets/icons/trending-up.svg";
+import TransactionsIcon from "../assets/icons/table.svg";
+import WalletIcon from "../assets/icons/wallet.svg";
+import ProfileIcon from "../assets/icons/user.svg";
 
 function HomePage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
   const userData = useSelector((state) => state.user.userData);
   const [cryptos, setCryptos] = useState([]);
   const [wallet, setWallet] = useState(null);
@@ -21,6 +27,14 @@ function HomePage() {
     dispatch(logout());
     navigate("/login");
   };
+
+  const menuItems = [
+    { label: "Home", path: "/home", icon: LayoutIcon },
+    { label: "Market", path: "/market", icon: MarketIcon },
+    { label: "Transactions", path: "/transactions", icon: TransactionsIcon },
+    { label: "Wallet", path: "/wallet", icon: WalletIcon },
+    { label: "Profile", path: "/profile", icon: ProfileIcon },
+  ];
 
   useEffect(() => {
     const fetchCryptos = async () => {
@@ -53,13 +67,44 @@ function HomePage() {
 
   return (
     <div className="flex h-screen w-full bg-black text-white font-sans">
-      <aside className="w-[350px] bg-black flex flex-col items-center py-6 space-y-4">
+      <aside className="w-[335px] bg-black flex flex-col items-center py-6 space-y-4">
         <h1 className="text-yellow-500 text-4xl font-bold tracking-wide">
           CRYPTO
         </h1>
         <img src={CryptoLogo} alt="Logo" className="w-[90px] h-[90px]" />
+
+        <nav className="mt-8 w-full flex flex-col gap-4 px-4">
+          {menuItems.map((item) => {
+            const isActive = location.pathname === item.path;
+
+            return (
+              <button
+                key={item.path}
+                onClick={() => navigate(item.path)}
+                className={`flex items-center gap-3 px-4 py-3 rounded-md font-semibold text-2xl transition
+            ${
+              isActive
+                ? "bg-zinc-900 text-white"
+                : "text-zinc-500 hover:bg-gray-300 hover:bg-opacity-30 hover:text-white"
+            }`}
+              >
+                <img
+                  src={item.icon}
+                  alt={`${item.label} icon`}
+                  className={`w-7 h-7 transition ${
+                    isActive
+                      ? "filter brightness-150 sepia hue-rotate-10 saturate-200"
+                      : ""
+                  }`}
+                />
+                {item.label}
+              </button>
+            );
+          })}
+        </nav>
       </aside>
 
+      {/* Main content */}
       <main className="flex-1 bg-gray-900 rounded-xl m-4 ml-2 p-6 overflow-y-auto flex flex-col items-center">
         <div className="mx-auto inline-block">
           <div className="flex justify-between items-center mb-6">
